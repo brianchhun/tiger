4e320ccadce16f4ee1af6a78ae37a9da0f84f652
gumchum <himself@brianchhun.org>
2016-01-31 12:08:57 -0600

    remove makefile since ocamlbuild works

--- ch2/driver.ml
+++ ch2/driver.ml
@@ -1,8 +1,9 @@
-try
-  let lexbuf = Lexing.from_channel stdin in
-  while true do
-    let result = Parser.prog Lexer.token lexbuf in
-    print_string result; print_newline(); flush stdout
-  done
-with Lexer.Eof ->
-  exit 0
+let _ =
+  try
+    let lexbuf = Lexing.from_channel stdin in
+    while true do
+      let result = Parser.prog Lexer.token lexbuf in
+      print_string result; print_newline(); flush stdout
+    done
+  with Lexer.Eof ->
+    exit 0
