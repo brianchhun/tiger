75950d5ed5b0ca2512f592bdafdb6901bc393d0c
gumchum <himself@brianchhun.org>
2016-02-07 13:50:43 -0600

    finish up lexer

--- ch2/driver.ml
+++ ch2/driver.ml
@@ -5,5 +5,5 @@ let _ =
       let result = Lexer.token lexbuf in
       print_string (Tokens.string_of_token result); print_newline(); flush stdout
     done
-  with Lexer.Eof ->
+  with Lexer.End_of_file ->
     exit 0
