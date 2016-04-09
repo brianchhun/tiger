568d0b6e54972fad4021d624b44459a5868d17e4
gumchum <himself@brianchhun.org>
2016-04-09 16:34:02 -0500

    translate absyn to ocaml

---
+++ ch4/symbol.mli
@@ -0,0 +1,7 @@
+type symbol
+val symbol : string -> symbol
+val name : symbol -> string
+type 'a table
+val empty : 'a table
+val enter : symbol -> 'a -> 'a table -> 'a table
+val look : symbol -> 'a table -> 'a option
